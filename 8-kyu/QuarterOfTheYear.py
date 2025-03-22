004bf4ce12347cfbee5d44f5bca778033d0c4fdc
JDLima-DS <j.davi4242@gmail.com>
2025-03-21 22:47:36 -0300

    add QuarterOfTheYear.py kata solution

---
+++ 8-kyu/QuarterOfTheYear.py
@@ -0,0 +1,9 @@
+def quarter_of(month):
+    if month >= 1 and month < 4:
+        return(1)
+    elif month >= 4 and month < 7:
+        return(2)
+    elif month >= 7 and month < 10:
+        return(3)
+    elif month >= 10 and month < 13:
+        return(4)
